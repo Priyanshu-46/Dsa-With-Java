41d01e4860e1c84ec1a12b97a9689c47bf2c7910
Priyanshu Patel <112636735+Priyanshu-46@users.noreply.github.com>
2025-12-31 11:36:55 +0530

    Time: 0 ms (100%), Space: 42.8 MB (19.88%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -1,19 +1,23 @@
 class Solution {
     public int balancedStringSplit(String s) {
-        int balance =0;
-        int count=0;
-        for(char ch:s.toCharArray()){
-            if(ch=='L'){
-                balance++;
+        int n=s.length();
+        int Rcount=0;
+        int Lcount=0;
+        int count=0; 
+            for(int j=0;j<n;j++){
+            if(s.charAt(j)=='R'){
+                Rcount++;
+            }else{
+                Lcount++;
             }
-            if(ch=='R'){
-                balance--;
-            }
-            if(balance==0){
+            if(Rcount==Lcount){
                 count++;
+                Rcount=0;
+                Lcount=0;
+            }
 
             }
-        }
+        
         return count;
     }
 }
